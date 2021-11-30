1843291ab94e23338a1aa54b612646b9663a4763
José Bernardo Johann <josebernardojohann@gmail.com>
2021-11-30 12:01:36 -0300

    Creates users table migration

---
+++ priv/repo/migrations/20211130145704_create_users_table.exs
@@ -0,0 +1,18 @@
+defmodule BlogsAPI.Repo.Migrations.CreateUsersTable do
+  @moduledoc false
+
+  use Ecto.Migration
+
+  def change do
+    create table :users do
+      add :displayName, :string
+      add :email, :string
+      add :password_hash, :string
+      add :image, :string
+
+      timestamps()
+    end
+
+    create unique_index(:users, [:email])
+  end
+end
