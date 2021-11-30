af612247a5f7710a5832863fda232df611eef6da
José Bernardo Johann <josebernardojohann@gmail.com>
2021-11-30 14:14:49 -0300

    Creates custom status code for e-mail already taken

--- lib/blogs_api/users/create.ex
+++ lib/blogs_api/users/create.ex
@@ -2,6 +2,7 @@ defmodule BlogsAPI.Users.Create do
   @moduledoc false
 
   alias BlogsAPI.{Error, Repo, User}
+  alias Ecto.Changeset
 
   def call(params) do
     params
@@ -11,6 +12,10 @@ defmodule BlogsAPI.Users.Create do
   end
 
   defp handle_insert({:ok, %User{}} = result), do: result
+
+  defp handle_insert({:error, %Changeset{errors: [email: {error, [constraint: :unique]}]}}) do
+    {:error, Error.build(:conflict, error)}
+  end
 
   defp handle_insert({:error, result}) do
     {:error, Error.build(:bad_request, result)}
