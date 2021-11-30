b18bb03a1a00aeed0a1ad236d187be1455dc4f0e
José Bernardo Johann <josebernardojohann@gmail.com>
2021-11-30 14:15:34 -0300

    Removes custom error by now

--- lib/blogs_api/users/create.ex
+++ lib/blogs_api/users/create.ex
@@ -13,10 +13,6 @@ defmodule BlogsAPI.Users.Create do
 
   defp handle_insert({:ok, %User{}} = result), do: result
 
-  defp handle_insert({:error, %Changeset{errors: [email: {error, [constraint: :unique]}]}}) do
-    {:error, Error.build(:conflict, error)}
-  end
-
   defp handle_insert({:error, result}) do
     {:error, Error.build(:bad_request, result)}
   end
