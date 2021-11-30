6f0934ddede5419326838ce9a5b0a137e6d2c84d
José Bernardo Johann <josebernardojohann@gmail.com>
2021-11-30 17:46:36 -0300

    Changes alias for users Get method

--- lib/blogs_api_web/auth/guardian.ex
+++ lib/blogs_api_web/auth/guardian.ex
@@ -4,18 +4,18 @@ defmodule BlogsAPIWeb.Auth.Guardian do
   use Guardian, otp_app: :blogs_api
 
   alias BlogsAPI.{Error, User}
-  alias BlogsAPI.Users.Get, as: UserGet
+  alias BlogsAPI.Users.Get, as: GetUser
 
   def subject_for_token(%User{id: id}, _claims), do: {:ok, id}
 
   def resource_from_claims(claims) do
     claims
     |> Map.get("sub")
-    |> UserGet.call()
+    |> GetUser.call()
   end
 
   def authenticate(%{"email" => email, "password" => password}) do
-    with {:ok, %User{password_hash: hash} = user} <- UserGet.by_email(email),
+    with {:ok, %User{password_hash: hash} = user} <- GetUser.by_email(email),
          true <- Bcrypt.verify_pass(password, hash),
          {:ok, token, _claims} <- encode_and_sign(user) do
       {:ok, token}
