84c8b8f8b33d11b0a5621d7d1e36c730b680cd0f
José Bernardo Johann <josebernardojohann@gmail.com>
2021-11-30 12:12:45 -0300

    Creates User schema

---
+++ lib/blogs_api/schemas/user.ex
@@ -0,0 +1,41 @@
+defmodule BlogsAPI.User do
+  @moduledoc false
+
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  @required_params [:displayName, :email, :password]
+
+  @derive {Jason.Encoder, except: [:password]}
+
+  schema "users" do
+    field :displayName, :string
+    field :email, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+    field :image, :string
+
+    timestamps()
+  end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_length(:displayName, min: 8)
+    |> validate_length(:password, min: 6)
+    |> validate_format(:email, ~r/(\w+)@([\w.]+)/)
+    |> unique_constraint([:email])
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(%Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Bcrypt.add_hash(password))
+  end
+
+  defp put_password_hash(changeset), do: changeset
+end
