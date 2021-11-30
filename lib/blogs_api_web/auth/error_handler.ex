e31dfa6594355e543c47b820e79bb2e82ddaafd2
José Bernardo Johann <josebernardojohann@gmail.com>
2021-11-30 17:46:15 -0300

    Implements Guardian errors handler

---
+++ lib/blogs_api_web/auth/error_handler.ex
@@ -0,0 +1,14 @@
+defmodule BlogsAPIWeb.Auth.ErrorHandler do
+  @moduledoc false
+
+  alias Guardian.Plug.ErrorHandler
+  alias Plug.Conn
+
+  @behaviour ErrorHandler
+
+  def auth_error(conn, {error, _reason}, _opts) do
+    body = Jason.encode!(%{message: to_string(error)})
+
+    Conn.send_resp(conn, 401, body)
+  end
+end
