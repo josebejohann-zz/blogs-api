32838dbef82728f13fb3b7c5844f87d8f1fe60ba
José Bernardo Johann <josebernardojohann@gmail.com>
2021-11-30 12:26:12 -0300

    Handles user insertion into database

---
+++ lib/blogs_api/users/create.ex
@@ -0,0 +1,18 @@
+defmodule BlogsAPI.Users.Create do
+  @moduledoc false
+
+  alias BlogsAPI.{Error, Repo, User}
+
+  def call(params) do
+    params
+    |> User.changeset()
+    |> Repo.insert()
+    |> handle_insert()
+  end
+
+  defp handle_insert({:ok, %User{}} = result), do: result
+
+  defp handle_insert({:error, result}) do
+    {:error, Error.build(:bad_request, result)}
+  end
+end
