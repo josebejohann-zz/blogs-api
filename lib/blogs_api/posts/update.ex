c8733355822410688217f9857962e63529752652
José Bernardo Johann <josebernardojohann@gmail.com>
2021-12-01 16:26:05 -0300

    Updates a post if done by its owner

---
+++ lib/blogs_api/posts/update.ex
@@ -0,0 +1,23 @@
+defmodule BlogsAPI.Posts.Update do
+  @moduledoc false
+
+  alias BlogsAPI.{Error, Post, Repo}
+
+  def call(%{"id" => id} = params) do
+    case Repo.get(Post, id) do
+      nil -> {:error, Error.post_not_found()}
+      post -> do_update(post, params)
+    end
+  end
+
+  defp do_update(post, params) do
+    post
+    |> Repo.preload([:user])
+    |> Post.changeset(params)
+    |> Repo.update()
+    |> handle_update()
+  end
+
+  defp handle_update({:ok, result}), do: {:ok, Repo.preload(result, [:user])}
+  defp handle_update({:error, reason}), do: {:error, Error.build(:bad_request, reason)}
+end
