44247c4483b4638dc82f4e7edd87c2afd55de2ab
José Bernardo Johann <josebernardojohann@gmail.com>
2021-12-01 16:26:12 -0300

    Deletes a post if done by its owner

---
+++ lib/blogs_api/posts/delete.ex
@@ -0,0 +1,12 @@
+defmodule BlogsAPI.Posts.Delete do
+  @moduledoc false
+
+  alias BlogsAPI.{Error, Repo, Post}
+
+  def call(id) do
+    case Repo.get(Post, id) do
+      nil -> {:error, Error.post_not_found()}
+      post -> Repo.delete(post)
+    end
+  end
+end
