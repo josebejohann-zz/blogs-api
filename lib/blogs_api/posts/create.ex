a9f988e27ee9cf2a2fd5f2a7c4b94a1c6e319846
José Bernardo Johann <josebernardojohann@gmail.com>
2021-12-01 16:25:35 -0300

    Creates a new post

---
+++ lib/blogs_api/posts/create.ex
@@ -0,0 +1,18 @@
+defmodule BlogsAPI.Posts.Create do
+  @moduledoc false
+
+  alias BlogsAPI.{Error, Post, Repo}
+
+  def call(params) do
+    params
+    |> Post.changeset()
+    |> Repo.insert()
+    |> handle_insert()
+  end
+
+  defp handle_insert({:ok, %Post{}} = result), do: result
+
+  defp handle_insert({:error, result}) do
+    {:error, Error.build(:bad_request, result)}
+  end
+end
