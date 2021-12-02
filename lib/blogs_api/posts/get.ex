9251dfd428f94bb67aad110cb9cbf1d8144d4eda
José Bernardo Johann <josebernardojohann@gmail.com>
2021-12-02 09:53:32 -0300

    Creates post get methods

---
+++ lib/blogs_api/posts/get.ex
@@ -0,0 +1,20 @@
+defmodule BlogsAPI.Posts.Get do
+  @moduledoc false
+  import Ecto.Query, only: [from: 2]
+
+  alias BlogsAPI.{Post, Repo}
+
+  def call() do
+    case Repo.all(from(p in Post, preload: [:user])) do
+      nil -> {:ok, []}
+      posts -> {:ok, posts}
+    end
+  end
+
+  def by_id(id) do
+    case Repo.get(Post, id) do
+      nil -> {:error, Error.post_not_found()}
+      post -> {:ok, post}
+    end
+  end
+end
